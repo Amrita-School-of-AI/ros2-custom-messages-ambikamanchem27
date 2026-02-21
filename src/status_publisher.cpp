7ecd7ee0e745ca81aaa747de69f3cb2f708ef0af
ambikamanchem27 <ambikamanchem27@gmail.com>
2026-02-21 09:36:31 +0530

    Fix repository structure for autograder

--- src/status_publisher.cpp
+++ src/status_publisher.cpp
@@ -1,52 +1,51 @@
-#include <chrono>
-#include <functional>
-#include <memory>
-#include <string>
-
 #include "rclcpp/rclcpp.hpp"
-// TODO: Include the generated message header
-// #include "ros2_custom_msgs/msg/robot_status.hpp"
+#include "ros2_custom_msgs/msg/robot_status.hpp"
 
 using namespace std::chrono_literals;
-
-/*
- * TODO: Create a Class named 'StatusPublisher' that inherits from rclcpp::Node.
- * Requirements:
- * 1. The constructor should name the node "status_publisher".
- * 2. Create a publisher to topic "/robot_status" with RobotStatus message type.
- * 3. Create a timer that triggers every 1000ms.
- * 4. In timer callback:
- *    - Set robot_name to "Explorer1"
- *    - Decrease battery_level from 100.0 by 0.5 each tick
- *    - Set is_active to true
- *    - Increment mission_count from 0
- *    - Publish and log the message
- */
 
 class StatusPublisher : public rclcpp::Node
 {
 public:
-    StatusPublisher()
-        : Node("status_publisher"), battery_level_(100.0), mission_count_(0)
-    {
-        // TODO: Create publisher here
+  StatusPublisher() : Node("status_publisher")
+  {
+    publisher_ = this->create_publisher<
+      ros2_custom_msgs::msg::RobotStatus>("/robot_status", 10);
 
-        // TODO: Create timer here
-    }
+    timer_ = this->create_wall_timer(
+      1s, std::bind(&StatusPublisher::publish_msg, this));
+  }
 
 private:
-    // TODO: Define timer_callback function here
+  void publish_msg()
+  {
+    ros2_custom_msgs::msg::RobotStatus msg;
+    msg.robot_name = "Explorer1";
+    msg.battery_level = battery_;
+    msg.is_active = true;
+    msg.mission_count = mission_count_;
 
-    rclcpp::TimerBase::SharedPtr timer_;
-    // TODO: Declare publisher
-    double battery_level_;
-    int32_t mission_count_;
+    publisher_->publish(msg);
+
+    RCLCPP_INFO(this->get_logger(),
+      "Robot=%s Battery=%.2f Missions=%d",
+      msg.robot_name.c_str(),
+      msg.battery_level,
+      msg.mission_count);
+
+    battery_ -= 1.0;
+    mission_count_++;
+  }
+
+  rclcpp::Publisher<ros2_custom_msgs::msg::RobotStatus>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  double battery_ = 100.0;
+  int mission_count_ = 0;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<StatusPublisher>());
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<StatusPublisher>());
+  rclcpp::shutdown();
+  return 0;
 }
